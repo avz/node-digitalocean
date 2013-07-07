fa48e26619e0a461d9136a0cb75175ba6c2ef5bc
avz <arepo@nologin.ru>
2013-07-07 16:32:04 +0400

    npm-ready fixes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-exports.Api = require('./lib/Api.js');
-exports.LowLevelApi = require('./lib/LowLevelApi.js');
+exports.Api = require('./lib/Api.js').Api;
+exports.LowLevelApi = require('./lib/LowLevelApi.js').LowLevelApi;
 
-exports.Error = require('./lib/exceptions/Error.js');
-exports.ApiError = require('./lib/exceptions/ApiError.js');
-exports.ArgumentError = require('./lib/exceptions/ArgumentError.js');
-exports.ResponseParserError = require('./lib/exceptions/ResponseParserError.js');
+exports.Error = require('./lib/exceptions/Error.js').Error;
+exports.ApiError = require('./lib/exceptions/ApiError.js').ApiError;
+exports.ArgumentError = require('./lib/exceptions/ArgumentError.js').ArgumentError;
+exports.ResponseParserError = require('./lib/exceptions/ResponseParserError.js').ResponseParserError;
