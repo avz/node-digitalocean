869506f933c3d3a1be813e5cb96125189ee0898d
avz <arepo@nologin.ru>
2013-07-21 14:30:03 +0400

    NotFoundError and AccessDeniedError now exporting

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ exports.LowLevelApi = require('./lib/LowLevelApi.js').LowLevelApi;
 
 exports.Error = require('./lib/exceptions/Error.js').Error;
 exports.ApiError = require('./lib/exceptions/ApiError.js').ApiError;
+exports.AccessDeniedError = require('./lib/exceptions/AccessDeniedError.js').AccessDeniedError;
+exports.NotFoundError = require('./lib/exceptions/NotFoundError.js').NotFoundError;
+
 exports.ArgumentError = require('./lib/exceptions/ArgumentError.js').ArgumentError;
 exports.ResponseParserError = require('./lib/exceptions/ResponseParserError.js').ResponseParserError;
